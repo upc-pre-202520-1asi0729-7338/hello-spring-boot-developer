650e7d22f789f9dbcfd505416d5a56b36fd274ea
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-06 16:57:57 -0500

    feat(greeting-developer): add inbound service for greeting a developer.

---
+++ src/main/java/com/acme/hello/platform/generic/interfaces/rest/controllers/GreetingsController.java
@@ -0,0 +1,37 @@
+package com.acme.hello.platform.generic.interfaces.rest.controllers;
+
+import com.acme.hello.platform.generic.domain.model.entity.Developer;
+import com.acme.hello.platform.generic.interfaces.rest.assemblers.DeveloperAssembler;
+import com.acme.hello.platform.generic.interfaces.rest.assemblers.GreetDeveloperResponseAssembler;
+import com.acme.hello.platform.generic.interfaces.rest.resources.GreetDeveloperRequest;
+import com.acme.hello.platform.generic.interfaces.rest.resources.GreetDeveloperResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping( name = "/api/v1/greetings", produces = APPLICATION_JSON_VALUE)
+public class GreetingsController {
+
+    @GetMapping
+    public ResponseEntity<GreetDeveloperResponse> greetDeveloper(
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName) {
+        Developer developer = (Objects.isNull(firstName) || Objects.isNull(lastName))
+                ? null
+                : Developer.builder().build().withFirstName(firstName).withLastName(lastName);
+        GreetDeveloperResponse response = GreetDeveloperResponseAssembler.toResponseFromEntity(developer);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<GreetDeveloperResponse> createGreeting(@RequestBody GreetDeveloperRequest request) {
+        Developer developer = DeveloperAssembler.toEntityFromRequest(request);
+        GreetDeveloperResponse response = GreetDeveloperResponseAssembler.toResponseFromEntity(developer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
